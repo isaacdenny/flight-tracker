cd31df34ba1ac983e2179bcaa8c35fea2fdf4437
Isaac Denny <isaacquinndenny@gmail.com>
2023-05-14 03:54:14 -0600

    error handling toggle flight

--- api/app/routers/inflight.py
+++ api/app/routers/inflight.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 import time
 import random
 
@@ -33,20 +33,26 @@ class FlightData():
         return { 'uuid': self.uuid, 'in_flight': self.in_flight, 'start_time': self.start_time, 'total_time': self.get_total_time()}
 
 
-in_flight_data = FlightData()
+in_flight_data = None
 
 @router.get("/")
 def get_flight_data():
     return in_flight_data.to_json()
 
 @router.post("/{id}/{command}")
-async def toggle_flight(id: int, command: str):
+async def toggle_flight(id: int, command: str, res: Response):
     global in_flight_data
     if command == 'start':
         in_flight_data = FlightData()
-        return { 'message': 'New flight started, id: ' + str(in_flight_data.get_id())}
+        return { 'message': f'New flight started, id: {in_flight_data.get_id()}'}
     elif command == 'end':
-        if in_flight_data.get_in_flight() and in_flight_data.get_id() == id:
+        if in_flight_data is None:
+            res.status_code = status.HTTP_400_BAD_REQUEST
+            return { 'error': 'No flights currently active' }
+        elif in_flight_data.get_id() != id or not in_flight_data.get_in_flight():
+            res.status_code = status.HTTP_400_BAD_REQUEST
+            return { 'error': 'Flight {id} not currently active' }
+        else:
             in_flight_data.set_in_flight(False)
             print(in_flight_data.to_json())
-            return { 'message': 'Flight ended, id: ' + str(in_flight_data.get_id())}
+            return { 'message': f'Flight ended, id: {in_flight_data.get_id()}'}
