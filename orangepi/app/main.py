5d63ef859de2ce644d7491ac64cea9fe1ca97f19
Isaac Denny <isaacquinndenny@gmail.com>
2023-05-14 17:28:55 -0600

    setup and startup

--- orangepi/app/main.py
+++ orangepi/app/main.py
@@ -10,6 +10,8 @@ server_ip = "192.168.0.10"
 server_port = 8000
 server_prefix = f"http://{server_ip}:{server_port}/"
 
+is_online = True
+
 velocity_ep = "velocity/"
 position_ep = "position/"
 in_flight_ep = "inflight/"
@@ -33,21 +35,25 @@ def request(endpoint, data=None):
     response_json = response.json()
     return response_json
 
-while True: # or is connected to internet
-    res = request(server_prefix + in_flight_ep)
-    in_flight = res['in_flight']
-    flight_time = res['flight_time']
+while is_online: # or is connected to internet
+    try:
+        res = request(server_prefix + in_flight_ep)
+        in_flight = res['in_flight']
+        flight_time = res['flight_time']
 
-    if not in_flight:
-        print("Flight status: Inactive")
-        time.sleep(1 / poll_rate)
-    else:
-        print("Flight status: Active")
-        print("Flight time: ", flight_time)
+        if not in_flight:
+            print("Flight status: Inactive")
+            time.sleep(1 / poll_rate)
+        else:
+            print("Flight status: Active")
+            print("Flight time: ", flight_time)
 
-        v_post_res = request(server_prefix + velocity_ep, velocity_data)
-        p_post_res = request(server_prefix + position_ep, position_data)
+            v_post_res = request(server_prefix + velocity_ep, velocity_data)
+            p_post_res = request(server_prefix + position_ep, position_data)
 
-        print("Updated Velocity: ", v_post_res)
-        print("Updated Position: ", p_post_res)
-        time.sleep(1 / in_flight_poll_rate)
+            print("Updated Velocity: ", v_post_res)
+            print("Updated Position: ", p_post_res)
+            time.sleep(1 / in_flight_poll_rate)
+    except:
+        print("Error requesting ip: " + server_ip)
+        time.sleep(2)
--- orangepi/setup.sh
+++
@@ -1,5 +0,0 @@
-!# /bin/bash
-
-python venv .venv 
-source .venv/bin/activate
-.venv/bin/pip install -r requirements.txt
