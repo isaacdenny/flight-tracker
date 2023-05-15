1f8ed6b8777c65d380cc18309b52830f71e386b1
Isaac Denny <isaacquinndenny@gmail.com>
2023-05-14 23:16:50 -0600

    docker-compose.yml and cors

--- api/app/main.py
+++ api/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import position, velocity, inflight
 
 app = FastAPI()
 app.include_router(position.router)
 app.include_router(velocity.router)
 app.include_router(inflight.router)
+
+origins = [
+    "http://192.168.0.20"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def get():
