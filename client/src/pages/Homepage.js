8f1d1602b730b3c61c9eee9025187076ab8356b3
Isaac Denny <isaacquinndenny@gmail.com>
2023-05-17 01:04:47 -0600

    altitude area chart

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,22 +1,59 @@
 import React, { useEffect, useState } from "react";
-import {
-  AreaChart,
-  YAxis,
-  XAxis,
-  Tooltip,
-  Area,
-} from "recharts";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
 const Homepage = () => {
-  const [data, setData] = useState([]);
+  const [options, setOptions] = useState({
+    chart: {
+      width: 800,
+    },
+    xAxis: {
+      labels: {
+        format: '{value:.1f}',
+      },
+      tickamount: "8",
+    },
+    title: {
+      text: `Altitude for flight: id here`,
+    },
+    series: [{ type: "area", name: "altitude", data: [] }],
+  });
 
   const fetchFlightInfo = async () => {
-    const time_req = await fetch((`http://localhost:8000/inflight/`))
-    const time_res = await time_req.json()
-    const position_req = await fetch((`http://localhost:8000/position/`))
-    const position_res = await position_req.json()
-    setData([...data, { 'time': time_res['total_time'], 'altitude': position_res['alt'] }]);
-  }
+    try {
+      const inflight_req = await fetch(`http://localhost:8000/inflight/`);
+      const inflight_res = await inflight_req.json();
+      const position_req = await fetch(`http://localhost:8000/position/`);
+      const position_res = await position_req.json();
+
+      if (inflight_res["in_flight"]) {
+        let updatedSeries = options.series;
+        updatedSeries[0].data.push([
+          inflight_res["total_time"],
+          position_res["alt"],
+        ]);
+        if (updatedSeries[0].data.length > 25) {
+          updatedSeries[0].data = updatedSeries[0].data.slice(1);
+        }
+        setOptions({
+          xAxis: {
+            labels: {
+              format: '{value:.1f}',
+            },
+            tickamount: "8",
+          },
+          title: {
+            text: `Altitude for flight: id here`,
+          },
+          series: updatedSeries,
+        });
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setTimeout(fetchFlightInfo, 3000);
+    }
+  };
 
   useEffect(() => {
     fetchFlightInfo();
@@ -25,30 +62,7 @@ const Homepage = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <AreaChart
-          width={730}
-          height={250}
-          data={data}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-        >
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis dataKey="time" />
-          <YAxis />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="altitude"
-            stroke="#8884d8"
-            fillOpacity={1}
-            fill="url(#colorUv)"
-          />
-        </AreaChart>
-        <button onClick={fetchFlightInfo}>Add to chart</button>
+        <HighchartsReact highcharts={Highcharts} options={options} />
       </header>
     </div>
   );
